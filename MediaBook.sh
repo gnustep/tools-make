c688a63b2e633c3275386bddb3ed2991f4664ada
Scott Christley <schristley@mac.com>
1999-02-18 06:22:33 +0000

    * opentool.in: New file. 	* rootinstall.sh: New file. 	* rootuninstall.sh: New file. 	* openapp.in: Add help text. 	* debugapp.in: Add help text. 	* configure.in: Add opentool.in. 	* configure: Regenerate. 	* GNUmakefile.in: Install/uninstall new files.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tools/make/trunk@3750 72102866-910b-0410-8b05-ffd578937521

--- MediaBook.sh
+++ MediaBook.sh
@@ -39,6 +39,7 @@ export MB_ORIG_PATH MB_PROD_LD_LIBRARY_PATH
 
 #
 # GCC environment variables
+# we will use this for hosting Project Center
 #
 #GCC_EXEC_PREFIX=$GNUSTEP_SYSTEM_ROOT/Libraries/$GNUSTEP_HOST_CPU/$GNUSTEP_HOST_OS/gcc-lib/
 #export GCC_EXEC_PREFIX
