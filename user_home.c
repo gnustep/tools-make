0506889dbe86e0b3e571366fbc4d634af4a53a73
Richard Frith-Macdonald <rfm@gnu.org>
2002-03-01 11:58:08 +0000

    Enhanced user_home
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tools/make/trunk@12918 72102866-910b-0410-8b05-ffd578937521

--- user_home.c
+++ user_home.c
@@ -29,6 +29,7 @@
 #endif
 
 #include <stdio.h>
+#include <ctype.h>
 
 #if defined(__MINGW__)
 # include <windows.h>
@@ -54,6 +55,32 @@
 # include <pwd.h>
 #endif
 
+#define lowlevelstringify(X) #X
+#define stringify(X) lowlevelstringify(X)
+
+/*
+ * This tool is intended to produce a definitive form of the
+ * user specific root directories for a GNUstep user.  It must
+ * remain consistent with the code in the GNUstep base library
+ * which provides path information for all GNUstep applications.
+ *
+ *
+ * How to run this tool ...
+ *
+ * 1. With no arguments ... the tool should print the home directory of
+ * the current user to stdout.
+ *
+ * 2. With a 'user' argument ... the tool should print the
+ * GNUSTEP_USER_ROOT directory to stdout.
+ *
+ * 3. With a 'defaults' argument ... the tool should print the
+ * GNUSTEP_DEFAULTS_ROOT directory to stdout.
+ *
+ * Any other arguments will be ignored.
+ * On success the tool will terminate with an exit status of zero
+ * On failure, the tool will terminate with an exit status of one
+ * and will print an error message to stderr.
+ */
 
 /* NOTE FOR DEVELOPERS.
  * If you change the behavior of this method you must also change
@@ -61,15 +88,40 @@
  */
 int main (int argc, char** argv)
 {
-#if defined(__MINGW__)
   char		buf0[1024];
+  char		path[2048];
+  char		home[2048];
+  char		*loginName = 0;
+  enum { NONE, DEFS, USER } type = NONE;
+#if defined(__MINGW__)
   char		buf1[1024];
   int		len0;
   int		len1;
 #else
   struct passwd *pw;
 #endif
-  char		*loginName = 0;
+
+  if (argc > 1)
+    {
+      if (strcmp(argv[1], "defaults") == 0)
+	{
+#ifdef	FORCE_DEFAULTS_ROOT
+	  printf("%s", stringify(FORCE_DEFAULTS_ROOT));
+	  return 0;
+#else
+	  type = DEFS;
+#endif
+	}
+      else if (strcmp(argv[1], "user") == 0)
+	{
+#ifdef	FORCE_USER_ROOT
+	  printf("%s", stringify(FORCE_USER_ROOT));
+	  return 0;
+#else
+	  type = USER;
+#endif
+	}
+    }
 
   if (loginName == 0)
     {
@@ -137,8 +189,7 @@ int main (int argc, char** argv)
       fprintf(stderr, "Unable to locate home directory for '%s'\n", loginName);
       return 1;
     }
-  printf("%s", pw->pw_dir);
-  return 0;
+  strncpy(home, pw->pw_dir, sizeof(home));
 #else
   /* Then environment variable HOMEPATH holds the home directory
      for the user on Windows NT; Win95 has no concept of home. */
@@ -146,16 +197,92 @@ int main (int argc, char** argv)
   if (len0 > 0 && len0 < 1024)
     {
       buf0[len0] = '\0';
-      len1 = GetEnvironmentVariable("HOMEDRIVE", buf1, 1024);
-      if (len1 > 0 && len1 < 1024)
+      len1 = GetEnvironmentVariable("HOMEDRIVE", buf1, 128);
+      if (len1 > 0 && len1 < 128)
 	{
 	  buf1[len1] = '\0';
-	  printf("%s%s", buf0, buf1);
-	  return 0;
+	  sprintf(home, "%s%s", buf0, buf1);
+	}
+      else
+	{
+	  fprintf(stderr, "Unable to determine HOMEDRIVE\n");
+	  return 1;
 	}
     }
-  fprintf(stderr, "Unable to determine HOMEDRIVE or HOMEPATH\n");
-  return 1;
+  else
+    {
+      fprintf(stderr, "Unable to determine HOMEPATH\n");
+      return 1;
+    }
 #endif
+
+  if (type == NONE)
+    {
+      strcpy(path, home);
+    }
+  else
+    {
+      FILE	*fptr;
+      char	*user = "";
+      char	*defs = "";
+
+      sprintf(path, "%s/.GNUsteprc", home);
+      fptr = fopen(path, "r");
+      path[0] = '\0';
+      if (fptr != 0)
+	{
+	  while (fgets(buf0, sizeof(buf0), fptr) != 0)
+	    {
+	      char	*pos = strchr(buf0, '=');
+
+	      if (pos != 0)
+		{
+		  char	*key = buf0;
+		  char	*val = pos;
+
+		  *val++ = '\0';
+		  while (isspace(*key))
+		    key++;
+		  while (strlen(key) > 0 && isspace(key[strlen(key)-1]))
+		    key[strlen(key)-1] = '\0';
+		  while (isspace(*val))
+		    val++;
+		  while (strlen(val) > 0 && isspace(val[strlen(val)-1]))
+		    val[strlen(val)-1] = '\0';
+
+		  if (strcmp(key, "GNUSTEP_USER_ROOT") == 0)
+		    {
+		      user = malloc(strlen(val)+1);
+		      strcpy(user, val);
+		    }
+		  else if (strcmp(key, "GNUSTEP_DEFAULTS_ROOT") == 0)
+		    {
+		      defs = malloc(strlen(val)+1);
+		      strcpy(user, val);
+		    }
+		}
+	    }
+	  fclose(fptr);
+	}
+      if (type == DEFS)
+	{
+	  strcpy(path, defs);
+	  if (*path == '\0')
+	    {
+	      strcpy(path, user);
+	    }
+	}
+      else
+	{
+	  strcpy(path, user);
+	}
+
+      if (*path == '\0')
+	{
+	  sprintf(path, "%s/GNUstep", home);
+	}
+    }
+  printf("%s", path);
+  return 0;
 }
 
