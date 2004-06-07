4dac07b5bededf0bcb889209f4b31ffbc8138a1b
Adam Fedor <fedor@gnu.org>
2004-06-07 14:54:18 +0000

    Reapply patch
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tools/make/trunk@19481 72102866-910b-0410-8b05-ffd578937521

--- ld_lib_path.sh
+++ ld_lib_path.sh
@@ -131,19 +131,15 @@ case "$host_os" in
       fi
     fi
     export DYLD_LIBRARY_PATH
-    if [ "$LIBRARY_COMBO" = "apple-apple-apple" -o \
-         "$LIBRARY_COMBO" = "apple" ]; then
-      if [ -z "$DYLD_FRAMEWORK_PATH" ]; then
-        DYLD_FRAMEWORK_PATH="$fw_paths"
-      else
-        if ( echo ${DYLD_FRAMEWORK_PATH}|
-             fgrep -v "${fw_paths}" >/dev/null ); then
-          DYLD_FRAMEWORK_PATH="$fw_paths:$DYLD_FRAMEWORK_PATH"
-        fi
+    if [ -z "$DYLD_FRAMEWORK_PATH" ]; then
+      DYLD_FRAMEWORK_PATH="$fw_paths"
+    else
+      if ( echo ${DYLD_FRAMEWORK_PATH}|
+           fgrep -v "${fw_paths}" >/dev/null ); then
+        DYLD_FRAMEWORK_PATH="$fw_paths:$DYLD_FRAMEWORK_PATH"
       fi
-      export DYLD_FRAMEWORK_PATH
     fi
-    ;;
+    export DYLD_FRAMEWORK_PATH;;
 
   *hpux*)
     if [ -z "$SHLIB_PATH" ]; then
